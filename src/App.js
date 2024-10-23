551549c7ce2d4c10e4d1745bf071dbd8b2c2bbec
WAFAA <wafaa@rakchamber.ae>
2024-10-23 13:29:58 +0400

    useEffect with empty array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [emotion, setEmotion] = useState("Happy");
+
+  useEffect(()=> {
+    console.log(`it's ${emotion} right now`)
+  },[]);
+
   return (
     <div className="App">
-      <h1>Welcome </h1>
+      <h1>{emotion} </h1>
+      <button onClick={()=> setEmotion('Sad')}>Sad</button>
+      <button onClick={()=> setEmotion('Excited')}>Excited</button>
+      <button onClick={()=> setEmotion('Happy')}>Happy</button>
     </div>
   );
 }
 
-export default App; //js 
+export default App; //js satement to be able to use it outside of this code 
