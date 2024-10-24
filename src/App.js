871d1ca9225aeab7afbfa4e78532d892f0f44ca9
AamnaAlshehhi <simplest25a@gmail.com>
2024-10-24 09:45:21 +0400

    create form spread

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
  
+
+
+function useInput(initialValue){
+
+  const [value , setValue] = useState(initialValue);
+  return[
+    {
+      value,
+      onChange: (e) => setValue(e.target.value)
+    },
+    ()=> setValue(initialValue)
+  ];
+
+}
+
+
+
 function App() {
  
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
 
 
@@ -13,18 +30,15 @@ function App() {
     const submit = (e) => {
       e.preventDefault();
 
-      alert(`${title},${color}`);
-      setTitle("");
-      setColor("#000000");
+      alert(`${titleProps.value}, ${colorProps.value}`);
+      resetTitle();
+      resetColor();
     }
  
   return (
     <form onSubmit= {submit}>
       <input
-        value={title}
-        onChange={(event) => 
-          setTitle(event.target.value)
-        }
+        {...titleProps}
         type="text"
         placeholder='color title ...'
       />
@@ -32,10 +46,7 @@ function App() {
 
 
     <input
-        value={color}
-        onChange={(event) => 
-          setColor(event.target.value)
-        }
+        {...colorProps}
         type="color"
         placeholder='#000000 ...'
       />
