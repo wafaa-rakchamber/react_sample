bc51402c203dac1ccd94ea7fcf96de7991443bb2
AamnaAlshehhi <simplest25a@gmail.com>
2024-10-24 11:25:03 +0400

    fetch data from our API

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
- 
+import { useState, useEffect } from 'react';
 
 
-function useInput(initialValue){
 
-  const [value , setValue] = useState(initialValue);
-  return[
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    ()=> setValue(initialValue)
-  ];
-
-}
 
 
 
 function App() {
- 
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
 
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    // fetch(`https://api.github.com/users/saadcs7`)
+    fetch(`https://chamber-test.com/ords/admin/list/currency`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
 
+  if (data)
+    return (
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>);
+  return <h1>Data </h1>
 
- 
-    const submit = (e) => {
-      e.preventDefault();
-
-      alert(`${titleProps.value}, ${colorProps.value}`);
-      resetTitle();
-      resetColor();
-    }
- 
-  return (
-    <form onSubmit= {submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder='color title ...'
-      />
-
-
-
-    <input
-        {...colorProps}
-        type="color"
-        placeholder='#000000 ...'
-      />
-      
-            <button>ADD</button>
- 
-    </form>
-  );
 }
 
 export default App;
