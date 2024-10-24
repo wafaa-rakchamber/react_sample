bd733880d569aa47e62fb9b5f4d706c67cc5b32b
WAFAA <wafaa@rakchamber.ae>
2024-10-24 09:45:54 +0400

    Custom Hooks - custome hook for Form Input Fields : useInput()

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import './App.css';
 import {useEffect, useReducer, useState} from "react";
 
+
+
+function useInput(initialValue){
+  const [value, setValue] = 
+    useState(initialValue);
+
+  return [
+    {
+      value,
+      onChange: (e) => setValue(e.target.value)
+    }, 
+    ()=> setValue(initialValue)
+  ];
+}
+
 function App() {
 
-    const [title, setTitle]= useState("");
-    const [color, setColor]= useState("#000000");
+    const [titleProps, resetTitle]= useInput("");
+    const [colorProps, resetColor]= useInput("#000000");
 
 
     const submit = (e) => {
       e.preventDefault();
-      alert(`${title},${color}`);
-      setTitle("");
-      setColor("#000000");
+      alert(`${titleProps.value},${colorProps.value}`);
+      resetTitle();
+      resetColor();
     }
 
 
   return (
     <form onSubmit= {submit}>
       <input
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
+        // value={title}
+        // onChange={(event) => setTitle(event.target.value)}
+        {...titleProps}
         type="text"
         placeholder='color title ...'
       />
       <input 
-      value={color} 
+      {...colorProps}
       type="color" 
-      onChange={(event)=>setColor(event.target.value)
-      }
       />
       <button>ADD</button>
 
