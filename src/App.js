80897136177304a3efbe7c16bce24d6dd81223f8
WAFAA <wafaa@rakchamber.ae>
2024-10-24 13:27:20 +0400

    REACT ROUTer

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 import {useEffect, useState} from "react";
 
-function GitHubUser({name, location, avatar}){
+function Home(){
   return(
     <div>
-      <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt={name} /> 
+      <nav>
+        <Link to='/about'>About</Link>
+        <Link to='/contactus'>Contact</Link>
+      </nav>
+      <h1>My Website</h1>
+      
+    </div>
+  );
+}
+export function About(){
+  return(
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contactus'>Contact</Link>
+      </nav>
+      <h1>About Us</h1>
+
+      
+    </div>
+  );
+}
+export function Contact(){
+  return(
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contactus'>Contact</Link>
+      </nav>
+      <h1>Contact US</h1>
+      
     </div>
   );
 }
 
-function App() {
 
-    const [data, setData]= useState(null);
-    const [username, setUserName] = useState("wafaa-rakchamber");
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    
-    useEffect(()=>{
-      setLoading(true);
-      fetch(
-        `http://api.github.com/users/${username}`
-      )
-      .then((response)=>response.json())
-      .then(setData)
-      .then(()=> setLoading(false))
-      .catch(setError);
-    },[]);
-
-
-    if(loading) return <h1>Loading ... </h1>;
-    if(error)
-      return <pre>{JSON.stringify(error)}</pre>;
-    if(data)
-      return (
-      <GitHubUser
-      name={data.login}
-      location={data.id}
-      avatar={data.avatar_url}
-      />
-    );
-      return  null;
+export function App() {
+      return  <Home />;
   
 }
 
