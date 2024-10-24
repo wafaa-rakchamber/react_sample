380e01242921d3d3d5e4117c73554461e8f5e16d
WAFAA <wafaa@rakchamber.ae>
2024-10-24 11:28:24 +0400

    Fetching data with Hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
 import './App.css';
-import {useEffect, useReducer, useState} from "react";
+import {useEffect, useState} from "react";
 
 
-
-function useInput(initialValue){
-  const [value, setValue] = 
-    useState(initialValue);
-
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    }, 
-    ()=> setValue(initialValue)
-  ];
-}
 
 function App() {
 
-    const [titleProps, resetTitle]= useInput("");
-    const [colorProps, resetColor]= useInput("#000000");
+    const [data, setData]= useState();
 
+    useEffect(()=>{
+      fetch(
+        `http://api.github.com/users/wafaa-rakchamber`
+      ).then((response)=>response.json())
+      .then(setData);
+    },[]);
 
-    const submit = (e) => {
-      e.preventDefault();
-      alert(`${titleProps.value},${colorProps.value}`);
-      resetTitle();
-      resetColor();
-    }
-
-
-  return (
-    <form onSubmit= {submit}>
-      <input
-        // value={title}
-        // onChange={(event) => setTitle(event.target.value)}
-        {...titleProps}
-        type="text"
-        placeholder='color title ...'
-      />
-      <input 
-      {...colorProps}
-      type="color" 
-      />
-      <button>ADD</button>
-
-    </form>
-  );
+    if(data)
+      return(
+        <pre>{JSON.stringify(data,null,2)}</pre>
+      );
+      return 
+        <h1>Data</h1>;
+  
 }
 
 export default App; //js satement to be able to use it outside of this code 
