1176c28e76708c607ad4b3b7ffb96a3cd88ac20f
WAFAA <wafaa@rakchamber.ae>
2024-10-24 11:49:23 +0400

    Handling Loading states - with function component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import './App.css';
 import {useEffect, useState} from "react";
 
-
+function GitHubUser({name, location, avatar}){
+  return(
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} /> 
+    </div>
+  );
+}
 
 function App() {
 
-    const [data, setData]= useState();
-  const [username, setUserName] = useState("wafaa-rakchamber");
+    const [data, setData]= useState(null);
+    const [username, setUserName] = useState("wafaa-rakchamber");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    
     useEffect(()=>{
+      setLoading(true);
       fetch(
         `http://api.github.com/users/${username}`
-      ).then((response)=>response.json())
-      .then(setData);
+      )
+      .then((response)=>response.json())
+      .then(setData)
+      .then(()=> setLoading(false))
+      .catch(setError);
     },[]);
 
+
+    if(loading) return <h1>Loading ... </h1>;
+    if(error)
+      return <pre>{JSON.stringify(error)}</pre>;
     if(data)
-      return(
-        <pre>{JSON.stringify(data,null,2)}</pre>
-      );
-      return 
-        <h1>Data</h1>;
+      return (
+      <GitHubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+      />
+    );
+      return  null;
   
 }
 
